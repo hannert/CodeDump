209304387ae93169a79f155ec762bdd1b314998b
hannert <79167052+hannert@users.noreply.github.com>
2021-11-01 21:10:48 -0400

    ArrayList to Array

--- hw4.java
+++ hw4.java
@@ -6,23 +6,38 @@ import java.util.ArrayList;
 
 class hw4{
 
-    public boolean finished = false;
+    static class dummyValue{
+        int val;
+        dummyValue(int n){
+            this.val = n;
+        }
+        public int getVal(){return this.val;}
+        
+        public void setVal(int n){this.val = n;}
+        public void addOneToVal(){this.val += 1;}
+    }
+    public static boolean finished = false;
     
-    public void backTrack(ArrayList<int[]> a, int k, ArrayList<int[]> input, int MAXCANDIDATES){
+    public static void backTrack(ArrayList<int[]> a, int k, ArrayList<int[]> input,
+                                int MAXCANDIDATES, int[] universalSet){
         int[][] c = new int[MAXCANDIDATES][];
-        int nc = 0;
+        dummyValue nc = new dummyValue(0);
+        
         int i;
 
-
-        if (isSolution(a, k, input)){
+        System.out.println("Backtrack!");
+        if (isSolution(a, k, input, universalSet)){
             processSolution();
         }
         else{
             k += 1;
             constructCandidates(a, k, input, c, nc, MAXCANDIDATES);     // updates c and nc
-            for (i = 0; i < nc; i++){
-                a.add(i, c[i]); // Comment
-                backTrack(a, k, input, MAXCANDIDATES);
+            System.out.println("nc: " + nc.getVal());
+            for (i = 0; i < nc.getVal(); i++){
+                System.out.println("constructed?!");
+                
+                a.set(i, c[i]); // Comment
+                backTrack(a, k, input, MAXCANDIDATES, universalSet);
                 if (finished) {
                     return;
                 }
@@ -30,12 +45,22 @@ class hw4{
         }
 
     }
-    
-    public boolean isSolution(ArrayList<int[]> a, int k, ArrayList<int[]> input){
-        
+    // ! Get all unique numbers and put in currentSolution to compare to universal set
+    public static boolean isSolution(ArrayList<int[]> a, int k, ArrayList<int[]> input, int[] universalSet){
+        ArrayList<Integer> currentSolution = new ArrayList<>();
+        for (int i = 0; i < a.size(); i++){ 
+            for (int j = 0; j < a.get(i).length; j++){
+                if(!currentSolution.contains(a.get(i)[j])){
+                    currentSolution.add(a.get(i)[j]);
+                }
+            }
+        }
+        for (int i = 0; i < universalSet.length; i++){
+            if(!currentSolution.contains(universalSet[i])) return false;
+        }
         return true;
     }
-    public void processSolution(){
+    public static void processSolution(){
         System.out.println("Done");
     }
     /**
@@ -47,21 +72,45 @@ class hw4{
      * @param nc    e
      * @param MAX   maximum size of answer set
      */
-    public void constructCandidates(ArrayList<int[]> a, int k, ArrayList<int[]> n, int[][] c, int nc, int MAX){
+    public static void constructCandidates(ArrayList<int[]> a, int k, ArrayList<int[]> n,
+                                     int[][] c, dummyValue nc, int MAX){
+        System.out.println("constructedCandidate!");
+
         int i;
         boolean inPerm[] = new boolean[MAX];
+        System.out.println("k: " + k);
 
         for (i = 1; i < k; i++){
+            System.out.println("i: " + i);
             inPerm[n.indexOf(a.get(i))] = true;
         }
+        System.out.println("Done with i");
+        for(i = 0; i < MAX; i++){
+            System.out.println(inPerm[i]);
+        }
+        
 
-        nc = 0;
-        for(i = 1; i <= n.size(); i++){
-            if (!inPerm[i]) { // ??????
-                c[nc] = n.get(i);
-                nc += 1;
+        nc.setVal(0);
+        for(i = 1; i < n.size(); i++){
+            System.out.println("k: " + i);
+
+            if (!inPerm[i]) { // ! ??????
+                c[nc.getVal()] = n.get(i);
+                nc.addOneToVal(); // nc++
+                System.out.println("nc: " + nc.getVal());
             }
         }
+        System.out.println("Done with n");
+        for(i = 0; i < nc.getVal(); i++){
+            System.out.println("First elem: " + c[i][0]);
+        }
+    }
+
+
+    public static void generatePermutations(ArrayList<int[]> masterSet, int MAX, int[] univ){
+        ArrayList<int[]> a = new ArrayList<int[]>();
+        
+        backTrack(a, 0, masterSet, MAX, univ);
     }
     public static void main(String[] args) throws FileNotFoundException{
         long startTime = System.currentTimeMillis();
@@ -72,7 +121,7 @@ class hw4{
         ArrayList<int[]> myList = new ArrayList<int[]>();
         File file = new File("s-X-12-6.txt");
         int lineNumber = 0;
-        int[] myArray;
+        int[] myArray = new int[0];
 
         try (BufferedReader br = new BufferedReader(new FileReader(file))){
             String line;
@@ -80,6 +129,10 @@ class hw4{
                 if (lineNumber == 0){
                     UNIVERSALSET = Integer.parseInt(line);
                     System.out.println("UNIV " + UNIVERSALSET);
+                    myArray = new int[UNIVERSALSET];
+                    for (int i = 0; i < UNIVERSALSET; i++){
+                        myArray[i] = i + 1;
+                    }
                 }
                 else if (lineNumber == 1) {
                     MAXCANDIDATES = Integer.parseInt(line);
@@ -102,15 +155,21 @@ class hw4{
                 //myArray[lineNumber - 2] = tempSet;
                 myList.add(tempSet);
                 lineNumber++;
-            }           
+            }
+
+            System.out.println("Universal Set: ");
+            for(int i = 0; i < myArray.length; i++){
+                System.out.print(myArray[i] + " ");
+            }
+            System.out.println();
             for(int i = 0; i < myList.size(); i++){
                 for(int j = 0; j < myList.get(i).length; j++){
                     System.out.print(myList.get(i)[j]);
                 }
                 System.out.println("");
             }
-
-                lineNumber++;
+            generatePermutations(myList, MAXCANDIDATES, myArray);
+                // lineNumber++;
             
         } catch(Exception e){
             System.out.println(e);
